cc1cb30e2adecb00c4b80cddc7f287d1dc395171
yann300 <yann.levreau@gmail.com>
2015-04-30 10:53:50 +0200

    register urlhint

--- registrar/urlhint.sol
+++ registrar/urlhint.sol
@@ -3,10 +3,19 @@
 // @authors:
 //   Gav Wood <g@ethdev.com>
 
+import "GlobalRegistrar";
+
 contract UrlHint {
 	struct Reg {
 		bytes32 url;
 		address owner;
+	}
+	
+	function UrlHint()
+	{
+		GlobalRegistrar reg = GlobalRegistrar(0xc6d9d2cd449a754c494264e1809c50e34d64562b);
+		reg.reserve("UrlHint");
+		reg.setAddress("UrlHint", this, true);
 	}
 
 	function url(bytes32 _hash) constant returns (bytes32) {
