d1c3b4329171c7d9ca7dbdee4cad195fc76d2e4e
chriseth <c@ethdev.com>
2015-05-13 19:04:47 +0200

    Revoke confirmation.

--- wallet/wallet2.sol
+++ wallet/wallet2.sol
@@ -44,6 +44,18 @@ contract multiowned {
         if (confirmed(_operation))
             _
     }
+    // Revokes a prior confirmation of the given operation
+    function revoke(bytes32 _operation) external {
+        uint ownerIndex = m_ownerIndex[msg.sender];
+        // make sure they're an owner
+        if (ownerIndex == 0) return;
+        uint ownerIndexBit = 2**ownerIndex;
+        var pending = m_pending[_operation];
+        if (pending.ownersDone & ownerIndexBit > 0) {
+            pending.yetNeeded++;
+            pending.ownersDone -= ownerIndexBit;
+        }
+    }
     function confirmed(bytes32 _operation) internal returns (bool) {
         // determine what index the present sender is:
         uint ownerIndex = m_ownerIndex[uint(msg.sender)];
