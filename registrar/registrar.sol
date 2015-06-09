8b79575a94fc5da28720db9c72da9003e69003ad
Gav Wood <i@gavwood.com>
2015-06-09 18:48:02 +0900

    Add FixedFeeRegistrar.

--- registrar/registrar.sol
+++ registrar/registrar.sol
@@ -6,18 +6,18 @@
 import "owned";
 
 contract NameRegistrar {
-	function getAddress(bytes32 _name) constant returns (address o_owner) {}
-	function getName(address _owner) constant returns (bytes32 o_name) {}
+	function addr(string _name) constant returns (address o_owner) {}
+	function name(address _owner) constant returns (string o_name) {}
 }
 
 contract Registrar is NameRegistrar {
-	event Changed(bytes32 indexed name);
-	event PrimaryChanged(bytes32 indexed name, address indexed addr);
+	event Changed(string indexed name);
+	event PrimaryChanged(string indexed name, address indexed addr);
 
-	function owner(bytes32 _name) constant returns (address o_owner) {}
-	function addr(bytes32 _name) constant returns (address o_address) {}
-	function subRegistrar(bytes32 _name) constant returns (address o_subRegistrar) {}
-	function content(bytes32 _name) constant returns (bytes32 o_content) {}
+	function owner(string _name) constant returns (address o_owner) {}
+	function addr(string _name) constant returns (address o_address) {}
+	function subRegistrar(string _name) constant returns (address o_subRegistrar) {}
+	function content(string _name) constant returns (bytes32 o_content) {}
 	
 	function name(address _owner) constant returns (bytes32 o_name) {}
 }
@@ -33,7 +33,7 @@ contract OwnedRegistrar is Registrar, owned {
 		return owner;
 	}
 
-	function disown(bytes32 _name) onlyowner {
+	function disown(string _name) onlyowner {
 		if (m_toName[m_toRecord[_name].primary] == _name)
 		{
 			PrimaryChanged(_name, m_toRecord[_name].primary);
@@ -43,7 +43,7 @@ contract OwnedRegistrar is Registrar, owned {
 		Changed(_name);
 	}
 
-	function setAddress(bytes32 _name, address _a, bool _primary) onlyowner {
+	function setAddress(string _name, address _a, bool _primary) onlyowner {
 		m_toRecord[_name].primary = _a;
 		if (_primary)
 		{
@@ -53,26 +53,95 @@ contract OwnedRegistrar is Registrar, owned {
 		else
 			Changed(_name);
 	}
-	function setSubRegistrar(bytes32 _name, address _registrar) onlyowner {
+	function setSubRegistrar(string _name, address _registrar) onlyowner {
 		m_toRecord[_name].subRegistrar = _registrar;
 		Changed(_name);
 	}
-	function setContent(bytes32 _name, bytes32 _content) onlyowner {
+	function setContent(string _name, bytes32 _content) onlyowner {
 		m_toRecord[_name].content = _content;
 		Changed(_name);
 	}
-	function record(bytes32 _name) constant returns (address o_primary, address o_subRegistrar, bytes32 o_content) {
+	function record(string _name) constant returns (address o_primary, address o_subRegistrar, bytes32 o_content) {
 		o_primary = m_toRecord[_name].primary;
 		o_subRegistrar = m_toRecord[_name].subRegistrar;
 		o_content = m_toRecord[_name].content;
 	}
-	function addr(bytes32 _name) constant returns (address) { return m_toRecord[_name].primary; }
-	function subRegistrar(bytes32 _name) constant returns (address) { return m_toRecord[_name].subRegistrar; }
-	function content(bytes32 _name) constant returns (bytes32) { return m_toRecord[_name].content; }
+	function addr(string _name) constant returns (address) { return m_toRecord[_name].primary; }
+	function subRegistrar(string _name) constant returns (address) { return m_toRecord[_name].subRegistrar; }
+	function content(string _name) constant returns (bytes32) { return m_toRecord[_name].content; }
 
-	function name(address _owner) constant returns (bytes32 o_name) { return m_toName[_owner]; }
+	function name(address _owner) constant returns (string o_name) { return m_toName[_owner]; }
 
-	mapping (address => bytes32) m_toName;
-	mapping (bytes32 => Record) m_toRecord;
+	mapping (address => string) m_toName;
+	mapping (string => Record) m_toRecord;
+}
+
+contract FixedFeeRegistrar is Registrar, owned {
+	struct Record {
+		address primary;
+		address subRegistrar;
+		bytes32 content;
+		address owner;
+	}
+
+	modifier onlyrecordowner(bytes32 _name) { if (m_toRecord[_name].owner == msg.sender) _ }
+
+	function reserve(string _name) {
+		if (m_toRecord[_name].owner == 0 && msg.value == c_fee) {
+			m_toRecord[_name].owner = msg.sender;
+			Changed(_name);
+		}
+	}
+	
+	function transfer(string _name, address _newOwner) onlyrecordowner(_name) {
+		m_toRecord[_name].owner = _newOwner;
+		Changed(_name);
+	}
+
+	function disown(string _name, address _refund) onlyrecordowner(_name) {
+		if (m_toName[m_toRecord[_name].primary] == _name)
+		{
+			PrimaryChanged(_name, m_toRecord[_name].primary);
+			delete m_toName[m_toRecord[_name].primary];
+		}
+		delete m_toRecord[_name];
+		_refund.send(c_fee);
+		Changed(_name);
+	}
+
+	function setAddress(string _name, address _a, bool _primary) onlyrecordowner(_name) {
+		m_toRecord[_name].primary = _a;
+		if (_primary)
+		{
+			PrimaryChanged(_name, _a);
+			m_toName[_a] = _name;
+		}
+		else
+			Changed(_name);
+	}
+	function setSubRegistrar(string _name, address _registrar) onlyrecordowner(_name) {
+		m_toRecord[_name].subRegistrar = _registrar;
+		Changed(_name);
+	}
+	function setContent(string _name, bytes32 _content) onlyrecordowner(_name) {
+		m_toRecord[_name].content = _content;
+		Changed(_name);
+	}
+	
+	function record(string _name) constant returns (address o_primary, address o_subRegistrar, bytes32 o_content) {
+		o_primary = m_toRecord[_name].primary;
+		o_subRegistrar = m_toRecord[_name].subRegistrar;
+		o_content = m_toRecord[_name].content;
+	}
+	function addr(string _name) constant returns (address) { return m_toRecord[_name].primary; }
+	function subRegistrar(string _name) constant returns (address) { return m_toRecord[_name].subRegistrar; }
+	function content(string _name) constant returns (bytes32) { return m_toRecord[_name].content; }
+	function owner(string _name) constant returns (address) { return m_toRecord[_name].owner; }
+
+	function name(address _owner) constant returns (string o_name) { return m_toName[_owner]; }
+
+	mapping (address => string) m_toName;
+	mapping (string => Record) m_toRecord;
+	int c_fee = 69 ether;
 }
 
