b673a6d929f823fa6f59b8ff4695eaaa67b8e2f4
chriseth <c@ethdev.com>
2015-07-29 18:01:58 +0200

    Fixes and optimisations for the fixed fee registrar.

--- registrar/FixedFeeRegistrar.sol
+++ registrar/FixedFeeRegistrar.sol
@@ -13,47 +13,52 @@ contract FixedFeeRegistrar is Registrar {
 		address owner;
 	}
 
-	modifier onlyrecordowner(string _name) { if (m_toRecord[_name].owner == msg.sender) _ }
+	modifier onlyrecordowner(string _name) { if (m_record(_name).owner == msg.sender) _ }
 
 	function reserve(string _name) {
-		if (m_toRecord[_name].owner == 0 && msg.value == c_fee) {
-			m_toRecord[_name].owner = msg.sender;
+	    Record rec = m_record(_name);
+		if (rec.owner == 0 && msg.value >= c_fee) {
+			rec.owner = msg.sender;
 			Changed(_name);
 		}
 	}
 	function disown(string _name, address _refund) onlyrecordowner(_name) {
-		delete m_toRecord[_name];
+		delete m_recordData[uint(sha3(_name)) / 8];
 		_refund.send(c_fee);
 		Changed(_name);
 	}
 	function transfer(string _name, address _newOwner) onlyrecordowner(_name) {
-		m_toRecord[_name].owner = _newOwner;
+		m_record(_name).owner = _newOwner;
 		Changed(_name);
 	}
 	function setAddr(string _name, address _a) onlyrecordowner(_name) {
-		m_toRecord[_name].addr = _a;
+		m_record(_name).addr = _a;
 		Changed(_name);
 	}
 	function setSubRegistrar(string _name, address _registrar) onlyrecordowner(_name) {
-		m_toRecord[_name].subRegistrar = _registrar;
+		m_record(_name).subRegistrar = _registrar;
 		Changed(_name);
 	}
 	function setContent(string _name, bytes32 _content) onlyrecordowner(_name) {
-		m_toRecord[_name].content = _content;
+		m_record(_name).content = _content;
 		Changed(_name);
 	}
 	
-	function record(string _name) constant returns (address o_addr, address o_subRegistrar, bytes32 o_content) {
-		o_addr = m_toRecord[_name].addr;
-		o_subRegistrar = m_toRecord[_name].subRegistrar;
-		o_content = m_toRecord[_name].content;
+	function record(string _name) constant returns (address o_addr, address o_subRegistrar, bytes32 o_content, address o_owner) {
+	    Record rec = m_record(_name);
+		o_addr = rec.addr;
+		o_subRegistrar = rec.subRegistrar;
+		o_content = rec.content;
+		o_owner = rec.owner;
 	}
-	function addr(string _name) constant returns (address) { return m_toRecord[_name].addr; }
-	function subRegistrar(string _name) constant returns (address) { return m_toRecord[_name].subRegistrar; }
-	function content(string _name) constant returns (bytes32) { return m_toRecord[_name].content; }
-	function owner(string _name) constant returns (address) { return m_toRecord[_name].owner; }
+	function addr(string _name) constant returns (address) { return m_record(_name).addr; }
+	function subRegistrar(string _name) constant returns (address) { return m_record(_name).subRegistrar; }
+	function content(string _name) constant returns (bytes32) { return m_record(_name).content; }
+	function owner(string _name) constant returns (address) { return m_record(_name).owner; }
 
-	mapping (string => Record) m_toRecord;
-	int c_fee = 69 ether;
+	Record[2**253] m_recordData;
+	function m_record(string _name) constant internal returns (Record storage o_record) {
+	    return m_recordData[uint(sha3(_name)) / 8];
+	}
+	uint constant c_fee = 69 ether;
 }
-
