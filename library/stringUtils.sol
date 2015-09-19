a08c48295e79c735ed00bd08d9eee8614c787cb3
Cyrus <c@mailcyr.us>
2015-09-19 11:49:28 -0400

    Added indexOf to StringUtils

--- library/stringUtils.sol
+++ library/stringUtils.sol
@@ -24,4 +24,32 @@ library StringUtils {
     function equal(string _a, string _b) returns (bool) {
         return compare(_a, _b) == 0;
     }
+    /// @dev Finds the first occurrence of _b in _a and returns its index or -1 if failure
+    function indexOf(string _a, string _b) returns (int) // _a = string to search, _b = string we want to find
+    {
+    	bytes memory a = bytes(_a);
+        bytes memory b = bytes(_b);
+        uint subindex = 0;
+    	if(a.length < 1 || b.length < 1 || (b.length > a.length)) 
+    		return -1;
+    	else if(a.length > (2**128 -1)) // since we have to be able to return -1 (if the char isn't found or input error), this function must return an "int" type with a max length of (2^128 - 1)
+    		return -1;									
+    	else
+    	{
+    		for (uint i = 0; i < a.length; i ++)
+    		{
+    			if (a[i] == b[0]) // found the first char of b
+    			{
+    				subindex = 1;
+    				while(subindex < b.length && (i + subindex) < a.length && a[i + subindex] == b[subindex]) // search until the chars don't match or until we reach the end of a or b
+    				{
+    					subindex++;
+    				}	
+    				if(subindex == b.length)
+    					return int(i);
+    			}
+    		}
+    		return -1;
+    	}	
+    }
 }
