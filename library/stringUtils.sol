8d054f42867040d355089ca28445cc7ecf056a7b
Cyrus <c@mailcyr.us>
2015-09-28 12:06:35 -0400

    Renamed variables to eliminate need for comment.

--- library/stringUtils.sol
+++ library/stringUtils.sol
@@ -24,28 +24,28 @@ library StringUtils {
     function equal(string _a, string _b) returns (bool) {
         return compare(_a, _b) == 0;
     }
-    /// @dev Finds the first occurrence of _b in _a and returns its index or -1 if failure
-    function indexOf(string _a, string _b) returns (int) // _a = string to search, _b = string we want to find
+    /// @dev Finds the index of the first occurrence of _needle in _haystack
+    function indexOf(string _haystack, string _needle) returns (int)
     {
-    	bytes memory a = bytes(_a);
-    	bytes memory b = bytes(_b);
-    	if(a.length < 1 || b.length < 1 || (b.length > a.length)) 
+    	bytes memory h = bytes(_haystack);
+    	bytes memory n = bytes(_needle);
+    	if(h.length < 1 || n.length < 1 || (n.length > h.length)) 
     		return -1;
-    	else if(a.length > (2**128 -1)) // since we have to be able to return -1 (if the char isn't found or input error), this function must return an "int" type with a max length of (2^128 - 1)
+    	else if(h.length > (2**128 -1)) // since we have to be able to return -1 (if the char isn't found or input error), this function must return an "int" type with a max length of (2^128 - 1)
     		return -1;									
     	else
     	{
     		uint subindex = 0;
-    		for (uint i = 0; i < a.length; i ++)
+    		for (uint i = 0; i < h.length; i ++)
     		{
-    			if (a[i] == b[0]) // found the first char of b
+    			if (h[i] == n[0]) // found the first char of b
     			{
     				subindex = 1;
-    				while(subindex < b.length && (i + subindex) < a.length && a[i + subindex] == b[subindex]) // search until the chars don't match or until we reach the end of a or b
+    				while(subindex < n.length && (i + subindex) < h.length && h[i + subindex] == n[subindex]) // search until the chars don't match or until we reach the end of a or b
     				{
     					subindex++;
     				}	
-    				if(subindex == b.length)
+    				if(subindex == n.length)
     					return int(i);
     			}
     		}
