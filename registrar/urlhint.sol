a523ebd5448477fa2eda65bc1dfd798c81174d28
Gav Wood <i@gavwood.com>
2015-10-10 12:39:25 +0200

    Various improvements.

--- registrar/urlhint.sol
+++ registrar/urlhint.sol
@@ -3,7 +3,7 @@
 // @authors:
 //   Gav Wood <g@ethdev.com>
 
-import "GlobalRegistrar";
+contract FixedFeeRegistrar{function name(address _owner)constant returns(string );function setOwner(string _name,address _newOwner);function setAddr(string _name,address _a);function addr(string _name)constant returns(address );function subRegistrar(string _name)constant returns(address );function reserve(string _name);function disown(string _name,address _refund);function setName(string _name);function setSubRegistrar(string _name,address _registrar);function content(string _name)constant returns(bytes32 );function owner(string _name)constant returns(address );function record(string _name)constant returns(address o_addr,address o_subRegistrar,bytes32 o_content,address o_owner);function setContent(string _name,bytes32 _content);}
 
 contract UrlHint {
 	struct Reg {
@@ -13,13 +13,27 @@ contract UrlHint {
 	
 	function UrlHint()
 	{
-		GlobalRegistrar reg = GlobalRegistrar(0xc6d9d2cd449a754c494264e1809c50e34d64562b);
-		reg.reserve("UrlHint");
-		reg.setAddress("UrlHint", this, true);
+		FixedFeeRegistrar reg = FixedFeeRegistrar(0x5e70c0bbcd5636e0f9f9316e9f8633feb64d4050);
+		reg.reserve.value(5 ether)("urlhinter");
+		reg.setAddr("urlhinter", this);
+		reg.setName("urlhinter");
+		reg.setOwner("urlhinter", msg.sender);
+	}
+
+	function kill() {
+		if (msg.sender == m_owner)
+		{
+			suicide(m_owner);
+			FixedFeeRegistrar(0x5e70c0bbcd5636e0f9f9316e9f8633feb64d4050).disown("urlhinter", m_owner);
+		}
 	}
 
 	function url(bytes32 _hash) constant returns (bytes32) {
 		return urls[_hash].url;
+	}
+
+	function owner(bytes32 _hash) constant returns (address) {
+		return urls[_hash].owner;
 	}
 
 	function suggestUrl(bytes32 _hash, bytes32 _url) {
@@ -29,6 +43,7 @@ contract UrlHint {
 		}
 	}
 	
+	address m_owner = msg.sender;
 	mapping (bytes32 => Reg) urls;
 }
 
