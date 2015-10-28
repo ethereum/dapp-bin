7e6c4c46bf592993440c6da55e6fbcfa663b38ae
chriseth <c@ethdev.com>
2015-10-28 17:34:44 +0100

    Change triple slash to double where it is not a natspec comment.

--- library/iterable_mapping.sol
+++ library/iterable_mapping.sol
@@ -59,20 +59,20 @@ library IterableMapping
   }
 }
 
-/// How to use it:
+// How to use it:
 contract User
 {
-  /// Just a struct holding our data.
+  // Just a struct holding our data.
   IterableMapping.itmap data;
-  /// Insert something
+  // Insert something
   function insert(uint k, uint v) returns (uint size)
   {
-    /// Actually calls itmap_impl.insert, auto-supplying the first parameter for us.
+    // Actually calls itmap_impl.insert, auto-supplying the first parameter for us.
     IterableMapping.insert(data, k, v);
-    /// We can still access members of the struct - but we should take care not to mess with them.
+    // We can still access members of the struct - but we should take care not to mess with them.
     return data.size;
   }
-  /// Computes the sum of all stored data.
+  // Computes the sum of all stored data.
   function sum() returns (uint s)
   {
     for (var i = IterableMapping.iterate_start(data); IterableMapping.iterate_valid(data, i); i = IterableMapping.iterate_next(data, i))
